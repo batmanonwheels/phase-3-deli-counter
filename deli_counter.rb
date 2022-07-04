ce61d9d12954d06a6af913a98aa2d87d96bf670a
Devin Garcia <devingarcia5423@gmail.com>
2022-07-04 16:29:12 -0400

    commit tree 64371d6152f6c1e9913558892e1eff4a0304fad8

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require "pry"
+
+katz_deli = ["Kat", "Kookie"]
+
+def line queue
+  if queue.empty?
+    puts "The line is currently empty."
+  else
+    str = 'The line is currently:'
+    queue.each.with_index(1) {|ele, index| str = str + " #{index}. #{ele}"}
+    puts str
+  end
+end
+
+def take_a_number (queue, name)
+  queue.push(name)
+  puts "Welcome, #{name}. You are number #{queue.length} in line."
+end
+
+def now_serving queue
+  if queue.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{queue.shift}."
+  end
+end
